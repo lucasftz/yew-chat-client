052b9d089425b05319eea11b3d019f666759e80f
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-23 19:09:32 -0500

    Add handlers to state

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 mod components;
 use components::login::Login;
-use gloo::console::log;
-use yew::{function_component, html, Callback};
+use yew::{function_component, html, use_state, Callback};
 
 #[function_component(App)]
 fn app() -> Html {
-    let is_logged_in = false; // hard coded for now
+    let nickname = use_state(|| String::default());
+    let is_logged_in = use_state(|| false);
+    let nickname_handler = nickname.clone();
+    let is_logged_in_handler = is_logged_in.clone();
 
-    let handle_login = Callback::from(|input| {
-        log!(input);
+    let handle_login = Callback::from(move |input| {
+        nickname.set(input);
+        is_logged_in.set(true);
     });
 
     html! {
             <main>
-            if is_logged_in {
-                <h1>{"User is signed in"}</h1>
+            if *is_logged_in_handler {
+                <h1>{&*nickname_handler}{" is signed in"}</h1>
             } else {
                 <Login handler={handle_login} />
             }
