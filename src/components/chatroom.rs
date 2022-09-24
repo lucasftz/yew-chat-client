df505a4338b902a8f106214f7beb72ba895bd3ed
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-24 02:06:20 -0500

    Remove shadows

--- src/components/chatroom.rs
+++ src/components/chatroom.rs
@@ -5,14 +5,16 @@ use yew::{function_component, html, use_context, Callback};
 #[function_component(ChatRoom)]
 pub fn chatroom() -> Html {
     let user = use_context::<User>().unwrap();
-    let user_shadow = user.clone();
 
-    let logout = Callback::from(move |_| user.log_out());
+    let logout = {
+        let user = user.clone();
+        Callback::from(move |_| user.log_out())
+    };
     let onsubmit = Callback::from(|_| ());
 
     html! {
         <>
-            <p>{"Username: "}{user_shadow.nick()}</p>
+            <p>{"Username: "}{user.nick()}</p>
             <button onclick={logout}>{"Log out"}</button>
             <Form handle_submit={onsubmit} error_msg={"".to_string()} />
         </>
--- src/components/form.rs
+++ src/components/form.rs
@@ -12,35 +12,38 @@ pub struct Props {
 #[function_component(Form)]
 pub fn form(props: &Props) -> Html {
     let handle_submit = props.handle_submit.clone();
-    // state
     let input_text = use_state(|| String::default());
     let is_error = use_state(|| false);
-    // shadows
-    let input_text_shadow = input_text.clone();
-    let is_error_shadow = is_error.clone();
 
-    let onsubmit = Callback::from(move |e: onsubmit::Event| {
-        e.prevent_default();
-        if input_text.is_empty() {
-            is_error.set(true);
-        } else {
-            handle_submit.emit(input_text.to_string())
-        }
-    });
+    let onsubmit = {
+        let input_text = input_text.clone();
+        let is_error = is_error.clone();
+        Callback::from(move |e: onsubmit::Event| {
+            e.prevent_default();
+            if input_text.is_empty() {
+                is_error.set(true);
+            } else {
+                handle_submit.emit(input_text.to_string())
+            }
+        })
+    };
 
-    let onchange = Callback::from(move |e: onchange::Event| {
-        let value = e
-            .target()
-            .unwrap()
-            .unchecked_into::<HtmlInputElement>()
-            .value();
-        input_text_shadow.set(value);
-    });
+    let onchange = {
+        let input_text = input_text.clone();
+        Callback::from(move |e: onchange::Event| {
+            let value = e
+                .target()
+                .unwrap()
+                .unchecked_into::<HtmlInputElement>()
+                .value();
+            input_text.set(value);
+        })
+    };
 
     html! {
         <form {onsubmit}>
             <input {onchange} type="text" /><br />
-            if *is_error_shadow && !props.error_msg.is_empty() {
+            if *is_error && !props.error_msg.is_empty() {
                 <p>{props.error_msg.as_str()}</p>
             }
             <input type="submit" value="Send" />
